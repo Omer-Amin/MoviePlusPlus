e66a18c6fbbd79d5d44134554a9a4b0c0a4f4b51
Omer-Amin <omer24@outlook.com>
2025-01-01 17:01:24 +0000

    README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A powerful and lightweight C++ 2d animation library.
+A powerful and lightweight C++ 2d animation library. MoviePlusPlus uses movie-making nomanclature to make reading and writing of code highly intuitive.
+
+
--- src/actor.h
+++ src/actor.h
@@ -53,8 +53,42 @@ struct Actor
         }
     }
 
-    void read(Script);
-    void setPosition(Vec2);
+    void read(Script acts)
+    {
+        for (auto& actions : acts)
+        {
+            for (auto& action : actions)
+            {
+                action.jumpTo(*this);
+            }
+        }
+        script = acts;
+    }
+    
+    void setPosition(Vec2 newPos)
+    {
+        const Vec2 deltaPosition = newPos - position;
+        position = newPos;
+        for (auto& vertex : vertices)
+            vertex = vertex + deltaPosition;
+    }
+    
     void afterTick(Actor&);
-    Actor reverse();
+    
+    Actor reverse()
+    {
+        Actor reversed = *this;
+        for (auto& actions : reversed.script)
+        {
+            for (auto& action : actions)
+            {
+                action = action.reverse();
+                action.jumpTo(reversed);
+            }
+            // std::reverse(actions.begin(), actions.end());
+        }
+        std::reverse(reversed.script.begin(), reversed.script.end());
+    
+        return reversed;
+    }
 };
